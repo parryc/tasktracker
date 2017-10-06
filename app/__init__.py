d4dde87ed1b50b5fda5cf1b23fbe258c83dd70cd
Parry Cadwallader <parrycadwallader@gmail.com>
2017-10-05 23:10:37 -0700

    Fix typo in login redirect

--- app/__init__.py
+++ app/__init__.py
@@ -41,7 +41,7 @@ app.register_blueprint(mod_users)
 @app.route('/')
 @login_required
 def index():
-  if current_user.user:
+  if current_user.username:
     return redirect(url_for("tasks.index"))
   else:
     return redirect(url_for("login"))
